4855be34606cd97138f1236e74fe196b472baf33
mokeshi3 <mokeshi34649>
2021-01-25 21:43:48 +0900

    [add] reference for array with declearing.

--- reference.pl
+++ reference.pl
@@ -14,3 +14,7 @@ my $rhash = \%hash;
 print "$$rscaler\n";
 print "@$rarray\n";
 print "%$rhash\n";
+
+# array reference
+my $ra = \('a', 'b', 'c');    # This reference is for scaler.
+print "$$ra\n";
